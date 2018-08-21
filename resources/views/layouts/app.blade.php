99c6c7f9de25a70bbb70a564e09120ac7cbe1839
tuanama13 <tuanama7@gmail.com>
2018-08-21 21:28:52 +0700

    Test Head

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
-    <title>CRUD LARAVE</title>
+    <title>CRUD LARAVEL</title>
     <style>
         .martop-sm {margin-top: 15px;}
         .martop-lg {margin-top: 70px;}
